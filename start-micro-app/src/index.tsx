2833dfde065b160a1e4aae3ae746635f18b40897
mnz8 <mnz8@foxmail.com>
2022-08-31 00:40:47 +0800

    start-micro-app: step one

--- start-micro-app/src/index.tsx
+++ start-micro-app/src/index.tsx
@@ -6,9 +6,29 @@ import routes from '../config/routes';
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container!);
 
-import microApp from "@micro-zoe/micro-app";
+import microApp from '@micro-zoe/micro-app';
 
-microApp.start();
+microApp.start({
+  plugins: {
+    modules: {
+      // appName即应用的name值
+      'svelte-child': [
+        {
+          loader(code) {
+            if (process.env.NODE_ENV === 'development') {
+              // 这里 basename 需要和子应用vite.config.js中base的配置保持一致
+              code = code.replace(/(from|import)(\s*['"])(\/basename\/)/g, (all) => {
+                return all.replace('/basename/', '子应用域名/basename/');
+              });
+            }
+
+            return code;
+          },
+        },
+      ],
+    },
+  },
+});
 
 const App: React.FC = () => {
   const element = useRoutes(routes);
--- start-micro-app/src/pages/SvelteChild.tsx
+++ start-micro-app/src/pages/SvelteChild.tsx
@@ -4,8 +4,10 @@ export default function ChildApp() {
       <h1>子应用</h1>
       <micro-app
         name="app1" // name(必传)：应用名称
-        url="http://localhost:5173/" // url(必传)：应用地址，会被自动补全为http://localhost:3000/index.html
-        baseroute="/child-app" // baseroute(可选)：基座应用分配给子应用的基础路由，就是上面的 `/my-page`
+        url="http://localhost:5173/svelte-child" // url(必传)：应用地址，会被自动补全为http://localhost:3000/index.html
+        // baseroute="/svelte-child" // baseroute(可选)：基座应用分配给子应用的基础路由，就是上面的 `/my-page`
+        inline // 使用内联script模式
+        disableSandbox // 关闭沙箱
       />
     </div>
   );
