6339ccdfe68c9ffff5a9fc3da04663aadcfabe51
mnz8 <mnz8@foxmail.com>
2023-12-19 17:58:10 +0800

    update: qwik v1 note: some api change

--- start-qwik/src/routes/layout.tsx
+++ start-qwik/src/routes/layout.tsx
@@ -1,16 +1,8 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { loader$ } from '@builder.io/qwik-city';
 
 import Header from '../components/header/header';
 
-export const useServerTimeLoader = loader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
-
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
   return (
     <>
       <main>
@@ -22,7 +14,6 @@ export default component$(() => {
       <footer>
         <a href="https://www.builder.io/" target="_blank">
           Made with ♡ by Builder.io
-          <div>{serverTime.value.date}</div>
         </a>
       </footer>
     </>
