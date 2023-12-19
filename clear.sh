faf8f43dbf57b6c34fb3ef5b35713e4460fc7efa
mnz8 <mnz8@foxmail.com>
2023-12-19 18:33:39 +0800

    rebuild: remix project

--- clear.sh
+++ clear.sh
@@ -7,3 +7,5 @@ find . -type d -name ".cache" -exec rm -rf {} \;
 find . -type d -name ".svelte-kit" -exec rm -rf {} \;
 find . -type d -name ".temp" -exec rm -rf {} \;
 find . -type d -name ".docusaurus" -exec rm -rf {} \;
+rm -rf start-remix/build \;
+rm -rf start-remix/public \;
